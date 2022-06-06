2d7ed25b033608d33e5fa0885403e9b523ac22a9
Flu3nt <gnedyalkov94@gmail.com>
2022-06-06 13:26:03 +0300

    Created a simple smart contract to test fallback() and receive() special functions in Solidity

---
+++ FallbackExample.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract FallbackExample {
+    uint256 public result;
+
+    receive() external payable {
+        result = 1;
+    }
+
+    fallback() external payable {
+        result = 2;
+    }
+}
+
+// Explainer from: https://solidity-by-example.org/sending-ether
+/*
+    Which function is called, fallback() or receive()?
+
+           send Ether
+               |
+         msg.data is empty?
+              / \
+            yes  no
+            /     \
+receive() exists?  fallback()
+         /   \
+        yes   no
+        /      \
+    receive()   fallback()
+    */
